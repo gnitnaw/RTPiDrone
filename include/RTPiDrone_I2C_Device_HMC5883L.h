f4833662b19ebaffdbec4aae2016894ff8d54315
gnitnaw <wanting@gmail.com>
2016-10-14 21:15:34 +0200

    Doing HMC5883L part, to be continued...

--- include/RTPiDrone_I2C_Device_HMC5883L.h
+++ include/RTPiDrone_I2C_Device_HMC5883L.h
@@ -1,8 +1,21 @@
 #ifndef H_RTPIDRONE_I2C_DEVICE_HMC5883L
 #define H_RTPIDRONE_I2C_DEVICE_HMC5883L
 
-#include "RTPiDrone_I2C_Device.h"
+/*!
+ * Drone_I2C_Device_HMC5883L class.
+ * \extends Drone_I2C_Device
+ */
+typedef struct Drone_I2C_Device_HMC5883L Drone_I2C_Device_HMC5883L;   //!< Drone_I2C_Device_HMC5883L type
+/*!
+ * Setup of HMC5883L.
+ * \public \memberof Drone_I2C_Device_HMC5883L
+ */
+int HMC5883L_setup(Drone_I2C_Device_HMC5883L**);
 
-int HMC5883L_init(Drone_I2C_Device*);
+/*!
+ * Remove of HMC5883L.
+ * \public \memberof Drone_I2C_Device_HMC5883L
+ */
+void HMC5883L_delete(Drone_I2C_Device_HMC5883L**);
 
 #endif
