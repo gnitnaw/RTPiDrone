5e4861c0e93b0397b1109396aea0eefa3405b3ec
gnitnaw <wanting@gmail.com>
2016-10-24 11:48:12 +0200

    doxygen test

--- include/RTPiDrone_I2C.h
+++ include/RTPiDrone_I2C.h
@@ -1,26 +1,17 @@
-/*
-    RTPiDrone -- RTPiDrone_I2C.h
-    Copyright 2016 Wan-Ting CHEN (wanting@gmail.com)
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ *   \file RTPiDrone_I2C.h
+ *   Copyright 2016 Wan-Ting CHEN (wanting@gmail.com)
+ */
 
 
 #ifndef  H_DRONE_I2C
 #define  H_DRONE_I2C
 
-typedef struct Drone_I2C    Drone_I2C;   //!< Drone_I2C type
+/*!
+ * \brief Drone_I2C type
+ * This is the structure which control all of the I2C devices
+ */
+typedef struct Drone_I2C    Drone_I2C;
 
 /*!
  * Initialize all I2C devices
@@ -28,17 +19,20 @@ typedef struct Drone_I2C    Drone_I2C;   //!< Drone_I2C type
  */
 int Drone_I2C_Init(Drone_I2C**);
 
+
 /*!
  * Calibrate all I2C devices
  * \public \memberof Drone_I2C
  */
 int Drone_I2C_Calibration(Drone_I2C*);
 
+
 /*!
  * Start all I2C devices
  * \public \memberof Drone_I2C
  */
 void Drone_I2C_Start(Drone_I2C*);
+
 
 /*!
  * Switch off all I2C devices
