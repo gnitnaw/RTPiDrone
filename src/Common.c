ca98ef5a8c4b9926c7024a75a376636de0ac6225
gnitnaw <wanting@gmail.com>
2016-10-29 22:25:12 +0200

    doxygen documents

--- src/Common.c
+++ src/Common.c
@@ -1,15 +1,18 @@
+/*!
+ * \file    Common.c
+ * \brief   Realization of the function defined in Common.h
+ */
 #include "Common.h"
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
 
 /*!
- * \fn      void exchange(char* buf, int len);
+ * \fn      void exchange(char* buf, int len)
  * \brief   Function to switch the endianness for 2-byte data (swap ith and (i+1)th, i = even)
  * \param buf Pointer where the data is
  * \param len How many (int16_t) data which the buf contains.
  */
-
 void exchange(char* buf, int len)
 {
     char tmp;
@@ -22,6 +25,11 @@ void exchange(char* buf, int len)
     }
 }
 
+/*!
+ * \fn      void _usleep(int micro)
+ * \brief   High-resolution sleep (with micro-second unit)
+ * \param micro Duration of sleep (micro-second)
+ */
 void _usleep(int micro)
 {
     struct timespec req = {0};
@@ -30,6 +38,12 @@ void _usleep(int micro)
     nanosleep(&req, (struct timespec *)NULL);
 }
 
+/*!
+ * \fn      float getSqrt(float* v, int N)
+ * \brief   Get the square-root of the square-sum : sqrt(v0*v0 + v1*v1 + ... + vN-1*vN-1)
+ * \param v Pointer of the data
+ * \param N How many elements which the data has.
+ */
 float getSqrt(float* v, int N)
 {
     float sum = 0.0f;
